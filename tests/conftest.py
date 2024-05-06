660136860e3ab7b6f08db504368a621a7a8bdb6b
Marvin van Aalst <marvin.vanaalst@gmail.com>
2024-05-06 09:48:00 +0200

    dev: chg: typing

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,16 +2,16 @@
 
 from __future__ import annotations
 
-from typing import List
+from typing import Callable
 
 import pytest
 
 
-def _multiline_comparison(expected: List[str], test: str):
+def _multiline_comparison(expected: list[str], test: str) -> None:
     for l1, l2 in zip(expected, test.split("\n")):
         assert l1 == l2
 
 
 @pytest.fixture()
-def multiline_comparison():
+def multiline_comparison() -> Callable[..., None]:
     return _multiline_comparison
