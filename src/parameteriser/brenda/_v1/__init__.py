3622bde6c28a969c5fe32e11eddb19e9eaaf6851
Marvin van Aalst <marvin.vanaalst@gmail.com>
2024-05-10 13:20:54 +0200

    dev: chg: removed unused code

--- src/parameteriser/brenda/_v1/__init__.py
+++ src/parameteriser/brenda/_v1/__init__.py
@@ -3,17 +3,14 @@ from __future__ import annotations
 import hashlib
 import json
 from dataclasses import asdict, dataclass, field
-from pathlib import Path
-from typing import Callable, TypedDict, TypeVar, cast
+from typing import TYPE_CHECKING, Callable, TypeVar
 
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-from parameteriser import _tex
-from parameteriser._paths import _default_cache_dir, _default_path
-from parameteriser._plot import plot_parameter_distributions, savefig
-from parameteriser._tex import export_tex_document
+from parameteriser._paths import _default_cache_dir
 from zeep import Client
+
+if TYPE_CHECKING:
+    from pathlib import Path
 
 
 @dataclass
@@ -141,112 +138,3 @@ class Brenda:
                 verbose=verbose,
             ),
         )
-
-
-class Describe(TypedDict):
-    count: int
-    mean: float
-    std: float
-
-
-def describe(s: pd.Series) -> Describe:
-    return {
-        "count": len(s),
-        "mean": s.mean(),
-        "std": s.std(),
-    }
-
-
-def routine(
-    organism: str,
-    ec: str,
-    substrate: str,
-    lower_percentile: int = 5,
-    upper_percentile: int = 95,
-) -> None:
-    with Path(".env").open() as fp:
-        cred: dict[str, str] = dict(
-            line.split("=", maxsplit=1) for line in fp.read().strip().split("\n")
-        )
-
-    TexPath = _default_path("tex")
-
-    brenda = Brenda(email=cred["EMAIL"], password=cred["PASSWORD"])
-    df = brenda.get_km(ec_number=ec)
-
-    tex: list[str] = []
-    all_kms = df["value"]
-    organism_kms = df[df["organism"] == organism]["value"]
-
-    all_kms_filtered = all_kms[
-        all_kms.between(
-            cast(float, np.percentile(all_kms, lower_percentile)),
-            cast(float, np.percentile(all_kms, upper_percentile)),
-        )
-    ]
-    organism_kms_filtered = organism_kms[
-        organism_kms.between(
-            cast(float, np.percentile(organism_kms, lower_percentile)),
-            cast(float, np.percentile(organism_kms, upper_percentile)),
-        )
-    ]
-
-    # tex.append(
-    #     stats.to_latex(
-    #         formatters={  # noqa: ERA001
-    #             "count": int,  # noqa: ERA001
-    #             "mean": partial(np.format_float_scientific, precision=1, trim="0"),  # noqa: ERA001
-    #             "std": partial(np.format_float_scientific, precision=1, trim="0"),  # noqa: ERA001
-    #         },
-    #     ),
-    # )  # noqa: ERA001
-
-    fig, ax = plot_parameter_distributions(
-        all_kms,
-        organism_kms,
-        ec=ec,
-        substrate=substrate,
-        organism_name=organism,
-    )
-    path = savefig(fig, f"km-{ec}-before-filtering", path=TexPath / "img")
-    tex.append(
-        _tex.figure(
-            path.relative_to(*path.parts[:1]),
-            caption="Distribution of Km values before any filtering was applied.",
-            label=f"fig:km-{ec}-before",
-            width=r"0.6\linewidth",
-        ),
-    )
-    plt.show()
-
-    fig, ax = plot_parameter_distributions(
-        all_kms_filtered,
-        organism_kms_filtered,
-        ec=ec,
-        substrate=substrate,
-        organism_name=organism,
-    )
-    path = savefig(fig, f"km-{ec}-after-filtering", path=TexPath / "img")
-    tex.append(
-        _tex.figure(
-            path.relative_to(*path.parts[:1]),
-            caption=(
-                f"Distribution of Km values after filtering out all values below "
-                rf"{lower_percentile} \% or above {upper_percentile} \% percentile."
-            ),
-            label=f"fig:km-{ec}-after",
-            width=r"0.6\linewidth",
-        ),
-    )
-    plt.show()
-
-    with (TexPath / "main.tex").open("w") as fp:
-        fp.write(export_tex_document("\n".join(tex), ""))
-
-
-if __name__ == "__main__":
-    routine(
-        organism="Nicotiana tabacum",
-        ec="4.1.1.39",
-        substrate="CO2",
-    )
